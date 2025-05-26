3f1e549aa5f9597860286150cae400d69cb45d41
rubayet <rubayetislam16@gmail.com>
2025-05-26 12:18:27 +0600

    some changes

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use TrodevIT\TroPay\Http\Controllers\BkashController;
 
 Route::get('/payment-type', [BkashController::class, 'index'])->name('tropay.bkash.pay');
+
+Route::redirect('/payment-type', '/tropay/pay');
 Route::prefix('tropay')->group(function () {
     Route::get('/pay', [BkashController::class, 'pay'])->name('tropay.pay');
     Route::post('/create-payment', [BkashController::class, 'createPayment'])->name('tropay.createPayment');
