52aa1f7d8c4bf647e1593c4c45b89d5cf95b4466
rubayet <rubayetislam16@gmail.com>
2025-05-26 11:26:37 +0600

    some changes

--- src/Http/Controllers/BkashController.php
+++ src/Http/Controllers/BkashController.php
@@ -19,7 +19,7 @@ class BkashController extends Controller
         $this->bkashClient = $bkashClient;
     }
 
-    public function index()
+    public static function index()
     {
         return View::make('paymentType');
     }
