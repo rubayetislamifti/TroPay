2209ef0cf12f8d4c9d2efb94fbe4ada3ab2f108b
rubayet <rubayetislam16@gmail.com>
2025-05-29 20:40:04 +0600

    some change

--- src/TroPayServiceProvider.php
+++ src/TroPayServiceProvider.php
@@ -37,11 +37,6 @@ class TroPayServiceProvider extends ServiceProvider
         $router = $this->app['router'];
         $router->aliasMiddleware('tropay', AppAuthMiddleware::class);
 
-        Route::middleware(['api', 'tropay'])->group(function () {
-            Route::post('/tropay/bkash/payment', [TroPayBkashController::class, 'initiate'])->name('tropay.payment');
-            Route::get('/tropay/bkash/callback', [TroPayBkashController::class, 'callback'])->name('tropay.callback');
-            Route::get('/tropay/bkash/query', [TroPayBkashController::class, 'query'])->name('tropay.query');
-        });
-
+        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use TrodevIT\TroPay\Http\Controllers\TroPayBkashController;
 
-//Route::middleware(['api', 'tropay'])->group(function () {
-//    Route::post('/tropay/bkash/payment', [TroPayBkashController::class, 'initiate'])->name('tropay.payment');
-//    Route::get('/tropay/bkash/callback', [TroPayBkashController::class, 'callback'])->name('tropay.callback');
-//    Route::get('/tropay/bkash/query', [TroPayBkashController::class, 'query'])->name('tropay.query');
-//});
+Route::middleware(['api', 'tropay'])->group(function () {
+    Route::post('/tropay/bkash/payment', [TroPayBkashController::class, 'initiate'])->name('tropay.payment');
+    Route::get('/tropay/bkash/callback', [TroPayBkashController::class, 'callback'])->name('tropay.callback');
+    Route::get('/tropay/bkash/query', [TroPayBkashController::class, 'query'])->name('tropay.query');
+});
