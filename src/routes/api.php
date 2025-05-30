7f7612c79469263f4b2580047becbce317f0ca82
rubayet <rubayetislam16@gmail.com>
2025-05-30 21:34:15 +0600

    some changes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use TrodevIT\TroPay\Http\Controllers\TroPayBkashController;
 use TrodevIT\TroPay\Middleware\AppAuthMiddleware;
 
-Route::middleware(AppAuthMiddleware::class)->group(function () {
+
 //    Route::get('/tropay/bkash/token', [TroPayBkashController::class, 'getToken'])->name('tropay.token');
     Route::post('/tropay/bkash/payment', [TroPayBkashController::class, 'initiate'])->name('tropay.payment');
     Route::get('/tropay/bkash/callback', [TroPayBkashController::class, 'callback'])->name('tropay.callback');
     Route::get('/tropay/bkash/query', [TroPayBkashController::class, 'query'])->name('tropay.query');
-});
+
