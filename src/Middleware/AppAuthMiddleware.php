df53b5417acd7f6c6c8bf5951f51382db03b5d0a
rubayet <rubayetislam16@gmail.com>
2025-05-30 21:31:14 +0600

    some changes

--- src/Middleware/AppAuthMiddleware.php
+++ src/Middleware/AppAuthMiddleware.php
@@ -20,15 +20,8 @@ class AppAuthMiddleware
         }
 
 
-        $info = DB::table('api_clients')->where(function ($query) use ($appKey, $appSecret) {
-            $query->where(function ($q) use ($appKey, $appSecret) {
-                $q->where('live_app_key', $appKey)
-                    ->where('live_app_secret', $appSecret);
-            })->orWhere(function ($q) use ($appKey, $appSecret) {
-                $q->where('sandbox_app_key', $appKey)
-                    ->where('sandbox_app_secret', $appSecret);
-            });
-        })->first();
+        $info = DB::table('api_clients')->where('live_app_key', $appKey)->where('live_app_secret',$appSecret)
+            ->orWhere('sandbox_app_key', $appKey)->where('sandbox_app_secret', $appSecret)->first();
 
         if (!$info) {
             return response()->json(['message' => 'Invalid credentials'], 401);
