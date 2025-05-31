b7b74f0fb649411e3adbd958a2829634abed568c
rubayet <rubayetislam16@gmail.com>
2025-05-31 11:32:45 +0600

    some changes

--- src/Helpers/Client.php
+++ src/Helpers/Client.php
@@ -55,7 +55,7 @@ class Client
                 ->post($url);
 
             if ($response->successful()) {
-                dd($response->json('id_token'));
+                return $response->json('id_token');
             }
         }
 
