618f46821982292dd47e62b0edce5560c2c03788
rubayet <rubayetislam16@gmail.com>
2025-05-31 13:26:12 +0600

    some

--- src/Http/Controllers/TroPayBkashController.php
+++ src/Http/Controllers/TroPayBkashController.php
@@ -11,28 +11,41 @@ class TroPayBkashController extends Controller
     protected $appKey;
     protected $appSecret;
     protected $credential;
-//    public function initiate(Client $bkashclient)
-//    {
-//
-//        $tokenData = $bkashclient->getToken();
-//
-////        dd($tokenData);
-//        return response()->json($tokenData);
-//    }
-    public function initiate(Request $request)
+    public function initiate(Client $bkashclient)
     {
-        $amount = $request->input('amount', 1); // Default amount 1
-        $bkash = new Client($request);
+        $this->appKey = request()->header('X-App-Key');
+        $this->appSecret = request()->header('X-App-Secret');
 
-        $paymentResult = $bkash->createPayment($amount);
-
-        if ($paymentResult['status'] && isset($paymentResult['data']['bkashURL'])) {
-            dd($paymentResult['data']['bkashURL']);
-            return redirect()->away($paymentResult['data']['bkashURL']);
+        if (!$this->appKey || !$this->appSecret) {
+            throw new \Exception('App Key and App Secret are required');
         }
 
-        return response()->json($paymentResult, 500);
+
+        $this->credential = DB::table('payment_infos')->where('provider', 'bkash')
+            ->join('api_clients', 'payment_infos.api_client_id', '=', 'api_clients.user_id')
+            ->select('payment_infos.*', 'api_clients.*')
+            ->first();
+
+        dd($this->appKey, $this->appSecret, $this->credential);
+        $tokenData = $bkashclient->getToken();
+
+//        dd($tokenData);
+        return response()->json($tokenData);
     }
+//    public function initiate(Request $request)
+//    {
+//        $amount = $request->input('amount', 1); // Default amount 1
+//        $bkash = new Client($request);
+//
+//        $paymentResult = $bkash->createPayment($amount);
+//
+//        if ($paymentResult['status'] && isset($paymentResult['data']['bkashURL'])) {
+//            dd($paymentResult['data']['bkashURL']);
+//            return redirect()->away($paymentResult['data']['bkashURL']);
+//        }
+//
+//        return response()->json($paymentResult, 500);
+//    }
 
     public function callback(Request $request)
     {
